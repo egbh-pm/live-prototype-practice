a250e4b35992a063b5bec6ba6bd961cbb7383f36
egbh-pm <emilygbieber@gmail.com>
2025-12-29 15:52:32 -0500

    Create RootLayout component with metadata

---
+++ app/layout.jslayout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Prototype Practice",
+  description: "Lightweight prototype practice",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>{children}</body>
+    </html>
+  );
+}
